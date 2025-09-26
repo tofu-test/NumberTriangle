3478caac6110e67c7698e0580e2dc76ab033dea9
sean <sean.ma456@gmail.com>
2025-09-26 18:29:05 -0400

    Completed retrieve method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,7 +88,20 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
+        // base case:
+        if (path.isEmpty()) {
+            return this.root;
+        }
+        
+        char direction = path.charAt(0);
+        
+        if (direction == 'l') {
+            return this.left.retrieve(path.substring(1));
+        } else if (direction == 'r') {
+            return this.right.retrieve(path.substring(1));
+        }
+        
+        // return -1 as fallback
         return -1;
     }
 
